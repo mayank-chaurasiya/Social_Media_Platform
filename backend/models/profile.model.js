f2ceaa50b2acd97646fb008aa23cb5ba2575e127
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-27 23:43:07 +0530

    Added Profile Model

--- backend/models/profile.model.js
+++ backend/models/profile.model.js
@@ -14,3 +14,45 @@ const educationSchema = new mongoose.Schema({
     default: "",
   },
 });
+
+const workSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    default: "",
+  },
+  position: {
+    type: String,
+    default: "",
+  },
+  years: {
+    type: String,
+    default: "",
+  },
+});
+
+const ProfileSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  currentPost: {
+    type: String,
+    default: "",
+  },
+  pastWork: {
+    type: [workSchema],
+    default: [],
+  },
+  education: {
+    type: [educationSchema],
+    default: [],
+  },
+});
+
+const Profile = mongoose.model("Profile", ProfileSchema);
+
+export default Profile;
