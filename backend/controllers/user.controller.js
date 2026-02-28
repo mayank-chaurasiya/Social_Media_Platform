2a7b8d9954bce74e8544b4b8afd01c8d372b5871
mayank-chaurasiya <try.maynk@gmail.com>
2026-03-01 00:29:16 +0530

    Testing with HTTP and Log in

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Profile from "../models/profile.model.js";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 export const register = async (req, res) => {
   try {
@@ -26,8 +27,27 @@ export const register = async (req, res) => {
 
     const profile = new Profile({ userId: newUser._id });
 
+    await profile.save();
+
     return res.json({ message: "User Created successfully" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    const token = crypto.randomBytes(32).toString("hex");
+    await User.updateOne({ _id: user._id }, { token });
+    return res.json({ token });
+  } catch (error) {}
 };
