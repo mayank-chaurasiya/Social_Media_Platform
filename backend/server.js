976142091e8db6406a5198e15507629cbe490103
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-28 16:53:46 +0530

    created user routes

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import path from "path";
-import { fileURLToPath } from "url";
 import postRoutes from "./routes/posts.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(postRoutes);
+app.use(userRoutes);
 
 const start = async () => {
   const mongoURI = process.env.MONGODB_URI;
