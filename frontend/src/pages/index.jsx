a6b5df1f1ca71752957ff24f7a632d4798387b59
mayank-chaurasiya <try.maynk@gmail.com>
2026-03-04 03:14:59 +0530

    applied styling of home page

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,28 @@
+import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+export default function Home() {
+  const router = useRouter();
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.mainContainer}>
+          <div className={styles.mainContainer__Left}>
+            <p>Connect with Friends without Exaggeration</p>
+            <p>A True social media platform, with stories no blufs.</p>
+            <div
+              onClick={() => {
+                router.push("/login");
+              }}
+              className={styles.buttonJoin}
+            >
+              <p>Join Now</p>
+            </div>
+          </div>
+          <div className={styles.mainContainer__Right}>
+            <img src="images/homemain_connection.avif" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "@/pages/login/style.module.css";
+
+const LoginComponent = () => {
+  return <div>LoginComponent</div>;
+};
+
+export default LoginComponent;
