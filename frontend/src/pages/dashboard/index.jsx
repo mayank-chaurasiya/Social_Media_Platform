6fb31afc30cea0cb4ff6abdd4e982c51cf8671be
mayank-chaurasiya <try.maynk@gmail.com>
2026-03-08 02:47:29 +0530

    implemented post options and like, share functions

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -3,6 +3,7 @@ import {
   createPost,
   getAllPosts,
   deletePost,
+  incrementPostLike,
 } from "@/config/redux/action/postAction";
 import UserLayout from "@/layout/UserLayout";
 import { useRouter } from "next/router";
@@ -147,6 +148,67 @@ const Dashboard = () => {
                           <p>{post.body}</p>
                           <div className={styles.singleCard__image}>
                             <img src={`${BASE_URL}/${post.media}`} alt="" />
+                          </div>
+                          <div className={styles.optionsContainer}>
+                            <div
+                              className={styles.singleOption__optionsContainer}
+                              onClick={async () => {
+                                await dispatch(
+                                  incrementPostLike({ post_id: post._id }),
+                                );
+                                dispatch(getAllPosts());
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="size-6"
+                              >
+                                <path d="M7.493 18.5c-.425 0-.82-.236-.975-.632A7.48 7.48 0 0 1 6 15.125c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75A.75.75 0 0 1 15 2a2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23h-.777ZM2.331 10.727a11.969 11.969 0 0 0-.831 4.398 12 12 0 0 0 .52 3.507C2.28 19.482 3.105 20 3.994 20H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 0 1-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227Z" />
+                              </svg>
+                              <p>{post.likes}</p>
+                            </div>
+                            <div
+                              className={styles.singleOption__optionsContainer}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="size-6"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </div>
+                            <div
+                              className={styles.singleOption__optionsContainer}
+                              onClick={() => {
+                                const text = encodeURIComponent(post.body);
+                                const url = encodeURIComponent(
+                                  "https://www.linkedin.com/in/mayank-kumar-anand/",
+                                );
+                                const twitterUrl = `https://twitter.com/intent/tweet?text=${text}&url=${url}`;
+                                window.open(twitterUrl, "_blank");
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="size-6"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </div>
                           </div>
                         </div>
                       </div>
