250be2517abd55a4075538d707fc3dc03979e6ba
mayank-chaurasiya <try.maynk@gmail.com>
2026-03-11 02:21:53 +0530

    working on user prfile page

--- frontend/src/pages/view_profile/[username].jsx
+++ frontend/src/pages/view_profile/[username].jsx
@@ -1,12 +1,44 @@
-import { clientServer } from "@/config";
+import { BASE_URL, clientServer } from "@/config";
+import DashboardLayout from "@/layout/DashboardLayout";
+import UserLayout from "@/layout/UserLayout";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
+import styles from "./userProfile.module.css";
 
 const ViewProfilePage = ({ userProfile }) => {
   const searchParameters = useSearchParams();
   useEffect(() => {});
 
-  return <div>{userProfile.userId.name}</div>;
+  return (
+    <UserLayout>
+      <DashboardLayout>
+        <div className={styles.container}>
+          <div className={styles.backDropContainer}>
+            <img
+              className={styles.backDrop}
+              src={`${BASE_URL}/${userProfile.userId.profilePicture}`}
+              alt=""
+            />
+          </div>
+          <div className={styles.profileContainer__details}>
+            <div className={styles.profileDetails__parent}>
+              <div className={styles.profileDetails__leftBar}>
+                <div className={styles.leftBar}>
+                  <p className={styles.profile__name}>
+                    {userProfile.userId.name}
+                  </p>
+                  <p className={styles.profile__username}>
+                    @{userProfile.userId.username}
+                  </p>
+                </div>
+              </div>
+              <div className={styles.profileDetails__rightBar}></div>
+            </div>
+          </div>
+        </div>
+      </DashboardLayout>
+    </UserLayout>
+  );
 };
 
 export async function getServerSideProps(context) {
